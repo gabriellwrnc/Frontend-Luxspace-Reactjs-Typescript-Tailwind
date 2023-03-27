13a6f389800f56959ad631d043d5cdc8435de5dd
Gabriel D Lawrence Jamrewav <gabriel.jamrewav@gmail.com>
2023-03-28 02:02:18 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/app.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='text-xl'>This isdsadsadsa main Hero</h1>
     </div>
   );
-}
+};
 
 export default App;
